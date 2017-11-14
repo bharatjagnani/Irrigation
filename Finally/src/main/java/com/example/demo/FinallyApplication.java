2070422cfccf4fc89c1aa93fd401ec7b525b0926
Kanha <Kanha@192.168.0.8>
2017-11-15 00:30:46 +0530

    first commit

---
+++ Finally/src/main/java/com/example/demo/FinallyApplication.java
@@ -0,0 +1,35 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan({"com.example.demo","controller"})
+public class FinallyApplication {
+
+	
+	public static void main(String[] args) {
+		SpringApplication.run(FinallyApplication.class, args);
+	}
+	
+	@Bean
+	public ViewResolver getViewResolver(){
+		
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setPrefix("/WEB-INF/jsp/");
+		resolver.setSuffix(".jsp");
+		resolver.setViewClass(JstlView.class);
+		return resolver;
+	} 
+}
---
+++ Finally/src/main/java/model/Person.java
@@ -0,0 +1,28 @@
+package model;
+
+public class Person {
+
+	String fstName;
+	String lstName;
+	int age;
+	public String getFstName() {
+		return fstName;
+	}
+	public void setFstName(String fstName) {
+		this.fstName = fstName;
+	}
+	public String getLstName() {
+		return lstName;
+	}
+	public void setLstName(String lstName) {
+		this.lstName = lstName;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+		
+
+}
